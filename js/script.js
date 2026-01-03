0171e2c84586ca526d2e99ff3a520cbfb5125d6a
Deepak Gehlot <72294553+deepgehlot@users.noreply.github.com>
2026-01-03 12:10:41 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -80,6 +80,26 @@
             onResized: numberDots
         });
 
+        $(".demo-slides").owlCarousel({
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 2
+                },
+                1000: {
+                    items: 3
+                }
+            },
+            loop: true,
+            margin: 30,
+            autoplay: true,
+            autoplayHoverPause: true,
+            smartSpeed: 1000,
+            dots: true
+        });
+
         // Before/After Slides Active Code
         $(".before-after-slides").owlCarousel({
             items: 1,
